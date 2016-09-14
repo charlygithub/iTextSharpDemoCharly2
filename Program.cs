6e1407b08782e2a600f0f5f176badf2028804d1c
charlygithub <carlosereynosog@hotmail.com>
2016-09-14 12:12:37 -0300

    Primera modificación
    Prueba de modificación

--- Program.cs
+++ Program.cs
@@ -8,12 +8,15 @@ namespace iTextSharpDemo
 {
     static class Program
     {
-        /// <summary>
+        /// <sumario>
         /// The main entry point for the application.
         /// </summary>
+		/// Esto es una prueba de modificación
+		static int numero;
         [STAThread]
         static void Main()
         {
+			numero = 10;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
